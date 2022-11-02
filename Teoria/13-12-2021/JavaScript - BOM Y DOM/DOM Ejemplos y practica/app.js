e4fd4d370548554b463375c49a0faf808e11364c
Mauro <soto.mauroa@gmail.com>
2022-11-02 18:02:56 -0300

    ejemplos de dom creando objetos

--- Teoria/13-12-2021/JavaScript - BOM Y DOM/DOM Ejemplos y practica/app.js
+++ Teoria/13-12-2021/JavaScript - BOM Y DOM/DOM Ejemplos y practica/app.js
@@ -3,6 +3,103 @@ let contenedor=document.querySelector("#root") //en este paso seleccionamos el c
 let titulo=document.createElement("h1") //en este paso crearmos una variable la cual guardara un elemento que es el que estamos creando, en este caso un h1
 titulo.innerText="Ejercicios de DOM" // en este paso le damos contenido a ese elemento creado, usando innertext
 
-contenedor.appendChild(titulo) // por ulitmo agregamos el elemento creado con su contenido al contenedor, como un hijo
+// contenedor.appendChild(titulo) // por ulitmo agregamos el elemento creado con su contenido al contenedor, como un hijo, ACLARACION, ESTO ES SOLO PARA 1 HIJO, SI SE NECESITA RENDERIZAR MAS DE 1 SE USA LA PROPIEDAD "append"
 
-//continua en 1.12
+//------------------------agregamos una imagen---------------------------
+
+let imagen=document.createElement("img")
+imagen.src=`bot-chat-decir-hola-robots-programados-hablar-clientes-linea_68708-622.webp` // con la propiedad src, podemos llamar imagenes, videos o archivos desde una direccion desde un servidor o localmente.
+imagen.width="200" // podemos aplicar las paropiedades de las imagenes en js para cambiarlas
+
+//--------------------creando una tabla solamente en js---------------------------
+
+let tabla=document.createElement("table")
+tabla.classList="table" //con la propiedad classlist podemos agregar clases de bootstrap a cualquier elemento
+tabla.innerHTML=` 
+<thead>
+<tr>
+<th>nombre</th>
+<th>Apellido</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+<td>Pablo</td>
+<td>Marino</td>
+</tr>
+<tr>
+<td>Fabrizio</td>
+<td>Tonin</td>
+</tr>
+<tr>
+<td>Pablo</td>
+<td>Frias</td>
+</tr>
+</tbody>` // la propiedad innerHTML nos permite agregar codigo de html a JS
+
+contenedor.append(titulo,imagen,tabla) // esta parte de codigo, donde se renderiza todo, siempre debe estar al final, por regla de la cascada
+const usuarios = [
+    {
+      id: 0,
+      email: "elmasca_pitos@hotmail.com",
+      first_name: "Micho",
+      last_name: "Talarga",
+      avatar: "https://reqres.in/img/faces/11-image.jpg",
+    },
+    {
+      id: 1,
+      email: "elmasca_pitos@hotmail.com",
+      first_name: "Micho",
+      last_name: "Talarga",
+      avatar: "https://reqres.in/img/faces/11-image.jpg",
+    },
+    {
+      id: 2,
+      email: "elmasca_pitos@hotmail.com",
+      first_name: "Micho",
+      last_name: "Talarga",
+      avatar: "https://reqres.in/img/faces/11-image.jpg",
+    },
+    {
+      id: 3,
+      email: "elmasca_pitos@hotmail.com",
+      first_name: "Micho",
+      last_name: "Talarga",
+      avatar: "https://reqres.in/img/faces/11-image.jpg",
+    },
+    {
+      id: 4,
+      email: "elmasca_pitos@hotmail.com",
+      first_name: "Micho",
+      last_name: "Talarga",
+      avatar: "https://reqres.in/img/faces/11-image.jpg",
+    },
+    {
+      id: 5,
+      email: "elmasca_pitos@hotmail.com",
+      first_name: "Micho",
+      last_name: "Talarga",
+      avatar: "https://reqres.in/img/faces/11-image.jpg",
+    },
+    {
+      id: 6,
+      email: "elmasca_pitos@hotmail.com",
+      first_name: "Micho",
+      last_name: "Talarga",
+      avatar: "https://reqres.in/img/faces/11-image.jpg",
+    },
+    {
+      id: 7,
+      email: "elmasca_pitos@hotmail.com",
+      first_name: "Micho",
+      last_name: "Talarga",
+      avatar: "https://reqres.in/img/faces/11-image.jpg",
+    },
+    {
+      id: 8,
+      email: "elmasca_pitos@hotmail.com",
+      first_name: "Micho",
+      last_name: "Talarga",
+      avatar: "https://reqres.in/img/faces/11-image.jpg",
+    },
+  ];
